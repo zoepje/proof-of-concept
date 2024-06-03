ef29dd97d860b118dce96835373b3ca6edc6e604
zoepje <zoe.penner@hva.nl>
2024-06-03 17:25:28 +0200

    Beginnetje opdracht

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log('Hier komt je server voor Sprint 12.')
+/*** Express setup & start ***/
+import fetchJson from './helpers/fetch-json.js'
+
+import express, { request, response } from 'express'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.set('views', './views')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true}))
+
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+const apiUrl = "https://potion-api-jet.vercel.app/",
+      potionsUrl = `${apiUrl}potions`,
+      ingredientsUrl = `${apiUrl}ingredients`;
+
+app.get('/', (request, response) => {
+  Promise.all([
+    fetchJson(`${potionsUrl}`),
+    fetchJson(`${ingredientsUrl}`)
+  ]).then(([potions, ingredients]) => {
+
+  response.render('index', {potions, ingredients });
+  })
+})
+
