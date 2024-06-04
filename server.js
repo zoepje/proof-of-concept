b6d9f71402d13dc46299f2d6551aa5f81324d9a8
TristanKatte <tristankatte@hotmail.com>
2024-06-04 16:11:15 +0200

    server alvast opgebouwd

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+import fetchJson from '.fetch-json.js'
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
