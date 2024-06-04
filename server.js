060d12a7288a0ff9067956063635bde3398c474f
zoepje <zoe.penner@hva.nl>
2024-06-04 12:56:26 +0200

    start code

--- server.js
+++ server.js
@@ -33,3 +33,64 @@ app.get('/', (request, response) => {
   })
 })
 
+app.get('/brew', (request, response) => {
+  Promise.all([
+    fetchJson(`${potionsUrl}`),
+    fetchJson(`${ingredientsUrl}`)
+  ]).then(([potions, ingredients]) => {
+
+  response.render('brew', {potions, ingredients });
+  })
+})
+
+app.post('/brew', (request, response) => {
+  fetchJson(`${potionsUrl}`).then((potions) => {
+    const ingredients = request.body.ingredients.map(Number);
+
+    function areEqual(arr1, arr2) {
+        let N = arr1.length;
+        let M = arr2.length;
+ 
+        // If lengths of array are not equal means
+        // array are not equal
+        if (N != M)
+            return false;
+ 
+        // Sort both arrays
+        arr1.sort();
+        arr2.sort();
+ 
+        // Linearly compare elements
+        for (let i = 0; i < N; i++)
+            if (arr1[i] != arr2[i])
+                return false;
+ 
+        // If all elements were same.
+        return true;
+    }
+
+    for (let i = 0; i < potions.length; i++){
+      if (areEqual(ingredients, potions[i].ingredients)){
+        response.redirect(301, '/')
+      } 
+    } 
+
+    response.status(500).send('Internal Server Error');
+    
+    // } else if (areEqual(ingredients, potions[1].ingredients)){
+    //   response.redirect(301, '/brew')
+    // } else {
+    //   response.status(500).send('Internal Server Error');
+    // }  
+  })
+})
+
+app.get('/potions', (request, response) => {
+  Promise.all([
+    fetchJson(`${potionsUrl}`),
+    fetchJson(`${ingredientsUrl}`)
+  ]).then(([potions, ingredients]) => {
+
+  response.render('potions', {potions, ingredients });
+  })
+})
