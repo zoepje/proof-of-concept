456ee75cb310cac341e9c0aa1b023d97a6eebcb2
TristanKatte <tristankatte@hotmail.com>
2024-06-07 13:07:00 +0200

    login klaargezet

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 console.log('Hier komt je server voor Sprint 12.')
 import express from 'express'
-import fetchJson from '.fetch-json.js'
+import fetchJson from './helpers/fetch-json.js'
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -11,3 +11,13 @@ app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+// get routes
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.get('/dashboard', function(request, response) {
+  response.render('dashboard');
+});
+
