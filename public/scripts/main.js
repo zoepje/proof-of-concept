d1071ca7f37a270550c6f5891a18cc5dc77754f6
zoepje <zoe.penner@hva.nl>
2024-06-11 17:02:07 +0200

    animation ingredients #16

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,9 @@
 const ingredientList = document.querySelector('.ingredient-list'),
       noItems = document.createElement('li'),
-      form = document.querySelector('#ingredient-form');
+      form = document.querySelector('#ingredient-form'),
+      cauldron = document.querySelector(".container img");
 
+// #region List
 // Function update de gekozen ingredienten
 function listIngredients() {
   const checkboxes = document.querySelectorAll('input[name="ingredients"]:checked');
@@ -23,6 +25,16 @@ function listIngredients() {
   }
 }
 
+// Doe een eventListener op elke checkbox wanneer er iets veranderd aan de input
+document.querySelectorAll('input[name="ingredients"]').forEach(checkbox => {
+    checkbox.addEventListener('change', listIngredients);
+});
+
+// Voor het begin van het laden
+listIngredients();
+// #endregion List
+
+// #region Alert
 // Functie check of er 2 of meer checkboxes gecheckd zijn
 function checked() {
   const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
@@ -46,11 +58,61 @@ form.addEventListener('submit', function(event) {
     event.preventDefault(); 
   }
 });
+// #endregion Alert
 
-// Doe een eventListener op elke checkbox wanneer er iets veranderd aan de input
-document.querySelectorAll('input[name="ingredients"]').forEach(checkbox => {
-    checkbox.addEventListener('change', listIngredients);
+// #region Animation
+function animateIngredient(checkbox, cauldron) {
+  const label = checkbox.nextElementSibling;
+  const img = label.querySelector("img");
+  const clonedImg = img.cloneNode(true);
+
+  document.body.appendChild(clonedImg)
+  clonedImg.classList.add("ingredient-duplicate");
+
+  // #region Paths
+  const ingredientRect = img.getBoundingClientRect();
+  const cauldronRect = cauldron.getBoundingClientRect();
+
+  const startX = ingredientRect.width / 2;
+  const startY = ingredientRect.height / 2;
+
+  const deltaX = cauldronRect.x - ingredientRect.x - (ingredientRect.width - cauldronRect.width) / 2;
+  const deltaY = cauldronRect.y - ingredientRect.y - (ingredientRect.height - cauldronRect.height) / 2;
+
+  const distance = Math.hypot(deltaX, deltaY);
+
+  const firstControlPointX = max(deltaX * .5, Math.sqrt(distance) * 4);
+  const firstControlPointY = deltaX * -.25;
+
+  const secondControlPointX = max(deltaX * .9, Math.sqrt(distance) * 6);
+  const secondControlPointY = deltaY * .5;
+
+  const path = `'m${startX},${startY} c${firstControlPointX},${firstControlPointY} ${secondControlPointX},${secondControlPointY} ${deltaX},${deltaY}'`;
+  clonedImg.style.setProperty("--path", path);
+  clonedImg.style.setProperty("--distance", distance);
+  // #endregion Paths
+  
+  // Zodat ze vanuit het label komen
+  clonedImg.style.left = ingredientRect.left + window.scrollX + ingredientRect.width / 2 + "px";
+  clonedImg.style.top = ingredientRect.top + window.scrollY + ingredientRect.height / 2 + "px";
+
+  clonedImg.addEventListener("animationend", function() {
+    clonedImg.remove();
+  })
+}
+
+function max(a, b) {
+	if (a > b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+form.addEventListener("change", function(event) {
+  const checkbox = event.target;
+  if (checkbox.type === "checkbox" && checkbox.checked) {
+    animateIngredient(checkbox, cauldron);
+  }
 });
-
-// Voor het begin van het laden
-listIngredients();
+// #endregion Animation
