d092f6ddbce1f7f922f73fb9e2b618a28851d020
TristanKatte <tristankatte@hotmail.com>
2024-06-13 14:40:56 +0200

    nav en afbeeldingen update

--- server.js
+++ server.js
@@ -56,3 +56,44 @@ app.get('/dashboard', async function(request, response) {
   response.render('dashboard', {data: apiResponse});
 });
 
+app.get('/google', async function(request, response) {
+	let propertyId = '301922918';
+
+	// Using a default constructor instructs the client to use the credentials
+	// specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+	const analyticsDataClient = new BetaAnalyticsDataClient();
+
+	// Runs a simple report.
+	const [res] = await analyticsDataClient.runReport({
+		property: `properties/${propertyId}`,
+		dateRanges: [
+		{
+			startDate: '2024-06-01',
+			endDate: 'today',
+		},
+		],
+		dimensions: [
+		{
+			name: 'country',
+		},
+		],
+		metrics: [
+		{
+			name: 'activeUsers',
+		},
+		],
+	});
+	response.render('google', {
+		rows: res.rows,
+	})
+})
+
+app.get('/linkedin', function(request, response) {
+
+  response.render('linkedin');
+});
+
+app.get('/hotjar', function(request, response) {
+
+  response.render('hotjar');
+});
